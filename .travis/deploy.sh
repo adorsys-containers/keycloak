ffdaece2613612e2d98b887c2a82b6b3c5726ba3
Jan-Otto Kröpke <jkr@adorsys.de>
2019-02-07 17:03:11 +0100

    Fix deployment

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -25,15 +25,3 @@ travis_retry() {
 
 echo "push to ${DOCKER_IMAGE}:${TAG}"
 travis_retry docker push "${DOCKER_IMAGE}:${TAG}"
-
-if [ -n "${ALIASES+x}" ]; then
-  echo "Pushing tag aliases ${ALIASES}"
-  docker tag "${DOCKER_IMAGE}:${TAG}" "${DOCKER_IMAGE}:${ALIASES}"
-  travis_retry docker push "${DOCKER_IMAGE}:${ALIASES}"
-fi
-
-if [ -n "${SNAPSHOT+x}" ] && [ "$(date +%d)" -eq "1" ]; then
-  echo "Pushing snapshot tag $(date +%Y%m)"
-  docker tag "${DOCKER_IMAGE}:${TAG}" "${DOCKER_IMAGE}:$(date +%Y%m)"
-  travis_retry docker push "${DOCKER_IMAGE}:$(date +%Y%m)"
-fi
